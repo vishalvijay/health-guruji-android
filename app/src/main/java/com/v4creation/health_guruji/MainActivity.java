71b145d92e096bc0ecedd2a4407a407763f0bff9
Vishal Vijay <0vishalvijay0@gmail.com>
2016-10-14 09:33:24 +0530

    Fix webview goback issue

--- app/src/main/java/com/v4creation/health_guruji/MainActivity.java
+++ app/src/main/java/com/v4creation/health_guruji/MainActivity.java
@@ -80,6 +80,8 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
+        } else if (webView.canGoBack()) {
+            webView.goBack();
         } else {
             super.onBackPressed();
         }
