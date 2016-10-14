99fc6a6230a274cb6b41d07b6f182b41f154a353
Vishal Vijay <0vishalvijay0@gmail.com>
2016-10-14 10:06:03 +0530

    Add crashlytics

--- app/src/main/java/com/v4creation/health_guruji/SplashActivity.java
+++ app/src/main/java/com/v4creation/health_guruji/SplashActivity.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
